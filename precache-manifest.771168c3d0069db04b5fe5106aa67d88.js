af131ef161c49ac5d4b4605619795fcefc68c6a5
ameedfaridi <faridiameed5@gmail.com>
2020-09-08 20:10:41 +0530

    Updates

---
+++ precache-manifest.771168c3d0069db04b5fe5106aa67d88.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e17ce0da2df84d0a858a2a5aa89a772",
+    "url": "/home-page/index.html"
+  },
+  {
+    "revision": "2fe348a4b0956e9a2f65",
+    "url": "/home-page/static/css/main.52c43f26.chunk.css"
+  },
+  {
+    "revision": "a7d8cf0df6fe70763932",
+    "url": "/home-page/static/js/2.d139db14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home-page/static/js/2.d139db14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe348a4b0956e9a2f65",
+    "url": "/home-page/static/js/main.a1e825ea.chunk.js"
+  },
+  {
+    "revision": "a8daefd333bc3b6c8353",
+    "url": "/home-page/static/js/runtime-main.e912c75d.js"
+  },
+  {
+    "revision": "67575db4b88fde878916e9f289f19707",
+    "url": "/home-page/static/media/BAP.67575db4.png"
+  },
+  {
+    "revision": "96bfa0179984148ffed597e87fc62ec9",
+    "url": "/home-page/static/media/BG.96bfa017.jpg"
+  },
+  {
+    "revision": "3a41e867bc3b41496d327882f37638f7",
+    "url": "/home-page/static/media/Bg2.3a41e867.png"
+  },
+  {
+    "revision": "c1ef55f681d2e30747909c5310a8c08f",
+    "url": "/home-page/static/media/HK.c1ef55f6.png"
+  },
+  {
+    "revision": "c24b8be856427af60690dd9308212e38",
+    "url": "/home-page/static/media/Images.c24b8be8.png"
+  },
+  {
+    "revision": "f74232de3f8743ae937582ed840f8125",
+    "url": "/home-page/static/media/SS.f74232de.png"
+  },
+  {
+    "revision": "aa768a29adc6b9d968f9bf7b4c6bb6ef",
+    "url": "/home-page/static/media/bev.aa768a29.png"
+  },
+  {
+    "revision": "f9fcb43951bfc8824602a2372598c4ad",
+    "url": "/home-page/static/media/fs.f9fcb439.png"
+  },
+  {
+    "revision": "051a81f0d9d7d604acecdf81079b184a",
+    "url": "/home-page/static/media/illustration.051a81f0.jpg"
+  },
+  {
+    "revision": "e53e9840f2e84e774075957081ece91a",
+    "url": "/home-page/static/media/responsive2.e53e9840.png"
+  },
+  {
+    "revision": "0dac81f7456a84457ab66f7ea4e2b003",
+    "url": "/home-page/static/media/staples.0dac81f7.png"
+  },
+  {
+    "revision": "b64ac43dad3d18046ea3ebf6b6e73476",
+    "url": "/home-page/static/media/thirdStepI.b64ac43d.png"
+  },
+  {
+    "revision": "f6d1a2552525edfc74ab43992e6e584c",
+    "url": "/home-page/static/media/veggies.f6d1a255.png"
+  }
+]);
